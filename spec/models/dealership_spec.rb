c8e6ac86456898ac63eab8c7eb7dcc788bf36b5c
Alex M <Alexm717@hotmail.com>
2022-08-30 17:02:41 -0700

    created test and method

---
+++ spec/models/dealership_spec.rb
@@ -0,0 +1,20 @@
+require "rails_helper"
+
+
+RSpec.describe(Dealership) do
+  before(:each) do
+    @dealership = Dealership.create!(    name: "the amazing dealership",     num_of_cars_on_lot: 3,     full: false)
+    @dealership2 = Dealership.create!(    name: "second dealership",     num_of_cars_on_lot: 3,     full: false)
+    @car1 = @dealership.cars.create!(    name: "Ford Focus",     color: "blue",     price: 5000,     miles: 123,     domestic: true)
+    @car2 = @dealership.cars.create!(    name: "Ram 1500",     color: "red",     price: 6000,     miles: 456,     domestic: true)
+    @car3 = @dealership.cars.create!(    name: "Toyota Corolla",     color: "yellow",     price: 7000,     miles: 789,     domestic: false)
+  end
+
+  xit("6.can order dealerships by most recently created") do
+    expect(Dealership.order_desc_created).to(eq([@dealership, @dealership2]))
+  end
+
+  it("7.it can count the number of childen(cars)") do
+    expect(@dealership.child_count).to(eq(3))
+  end
+end
