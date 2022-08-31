70a2f6a4cf2d253e9b988e5a219adba420108a47
Alex M <Alexm717@hotmail.com>
2022-08-30 20:57:04 -0700

    story 10 complete

--- spec/features/dealerships/show_spec.rb
+++ spec/features/dealerships/show_spec.rb
@@ -22,4 +22,15 @@ RSpec.describe("dealership show", type: :feature) do
     expect(page).to(have_content(dealership.num_of_cars_on_lot))
     expect(page).to(have_content(dealership.full))
   end
+
+  it("10.visit a parent show page ('/parents/:id')Then link to ('/parents/:id/child_table_name') ") do
+    dealership = Dealership.create!(    name: "the amazing dealership",     num_of_cars_on_lot: 3,     full: false)
+    car1 = dealership.cars.create!(    name: "Ford Focus",     color: "blue",     price: 5000,     miles: 123,     domestic: true)
+    car2 = dealership.cars.create!(    name: "Ram 1500",     color: "red",     price: 6000,     miles: 456,     domestic: true)
+    car3 = dealership.cars.create!(    name: "Toyota Corolla",     color: "yellow",     price: 7000,     miles: 789,     domestic: false)
+    visit("/dealership/#{dealership.id}")
+    expect(page).to(have_button("Cars Available"))
+    click_on("Cars Available")
+    expect(current_path).to(eq("/dealership/#{dealership.id}/cars"))
+  end
 end
