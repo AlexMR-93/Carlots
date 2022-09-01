aa1073866350038f7fb095d8765624a24f1c523a
Alex M <Alexm717@hotmail.com>
2022-08-31 21:22:39 -0700

    story eleven complete

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,12 @@
 Rails.application.routes.draw do
   get("/",   to: "dealerships#index")
   get("/dealerships",   to: "dealerships#index")
-  get("/dealership/:id",   to: "dealerships#show")
+  get("/dealerships/new",   to: "dealerships#new")
+  post("/dealerships",   to: "dealerships#create")
+  get("dealerships/:id/edit",   to: "dealerships#edit")
+  get("dealerships/:id",   to: "dealerships#update")
+  get("/dealerships/:id",   to: "dealerships#show")
   get("/cars",   to: "cars#index")
   get("/cars/:id",   to: "cars#show")
-  get("/dealership/:id/cars",   to: "dealership_cars#index")
+  get("/dealerships/:id/cars",   to: "dealership_cars#index")
 end
